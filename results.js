accec590882c783c059375a2e8d7742401bce2af
Max Walters <max.walters10@gmail.com>
2018-05-25 00:20:47 -0400

    Added audio links

--- results.js
+++ results.js
@@ -1,10 +1,22 @@
 getResults = function(){
-  var audioLinks = []
+  var audioLinks = ["https://s3.amazonaws.com/media.cloutclinic.fuego/response/assessment.mp3", "https://s3.amazonaws.com/media.cloutclinic.fuego/response/critically+low.mp3",
+  "https://s3.amazonaws.com/media.cloutclinic.fuego/response/god+tier.mp3", "https://s3.amazonaws.com/media.cloutclinic.fuego/response/more+serious.mp3", "https://s3.amazonaws.com/media.cloutclinic.fuego/response/not+fuego.mp3",
+  "https://s3.amazonaws.com/media.cloutclinic.fuego/response/one+like+one+prayer.mp3", "https://s3.amazonaws.com/media.cloutclinic.fuego/response/skyrocketing.mp3",
+  "https://s3.amazonaws.com/media.cloutclinic.fuego/response/so+proud.mp3", "https://s3.amazonaws.com/media.cloutclinic.fuego/response/whos+mans.mp3",
+  "https://s3.amazonaws.com/media.cloutclinic.fuego/response/your+vision.mp3"];
+
+  
 }
 
 /** Link List
-1.
-2.
-3.
-
+1. Assessment
+2. Critically Low
+3. God Tier
+4. More Serious
+5. Not Fuego
+6. One Like One Prayer
+7. Skyrocketing
+8. So Proud
+9. Whos Mans
+10. Your Vision
 **/
